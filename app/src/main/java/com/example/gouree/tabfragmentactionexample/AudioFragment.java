1cf5e1128ab5b55f413b5a1dd9ea58888c4718f6
goureemanjarekar <goure.deshpande@gmail.com>
2017-09-27 19:46:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/gouree/tabfragmentactionexample/AudioFragment.java
@@ -0,0 +1,26 @@
+package com.example.gouree.tabfragmentactionexample;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by omsai on 27/09/2017.
+ */
+
+public class AudioFragment extends Fragment
+{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
+    {
+        //return the inflated layout
+        return inflater.inflate(R.layout.audio,container,false);
+    }
+}
